64a1cf81d684be1fef590f0a1fcefb2d7e8f8a12
Nishant Zoc <zocnishant@gmail.com>
2024-11-01 19:46:27 +0545

    .

--- choicenepal_website/src/components/Gallery/Gallery.jsx
+++ choicenepal_website/src/components/Gallery/Gallery.jsx
@@ -1,37 +1,4 @@
-// import React from "react";
-// import "./Gallery.css";
-// import balithum from "../../assets/balithum.jpg";
-// import projectPics from "../../assets/hero.jpg";
-// import cowshed from "../../assets/cowshed.jpg";
-// import waterTap from "../../assets/waterTap.jpg";
-// import expedition from "../../assets/expedition.jpg";
-
-// const Gallery = () => {
-//   return (
-//     <>
-//       <div id="gallery" className="choiceGallery">
-//         <div className="gallery">
-//           <img src={balithum} alt="" />
-//           <img src={cowshed} alt="" />
-//           <img src={waterTap} alt="" />
-//           <img src={projectPics} alt="" />
-//         </div>
-//         <div className="gallery">
-//           <img src={projectPics} alt="" />
-//           <img src={projectPics} alt="" />
-//           <img src={projectPics} alt="" />
-//           <img src={expedition} alt="" />
-//         </div>
-
-//         <button className="btn darkBtn">See More Pictures</button>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Gallery;
-
-import React, { useState } from "react";
+import { useState } from "react";
 import "./Gallery.css";
 import balithum from "../../assets/balithum.jpg";
 import projectPics from "../../assets/hero.jpg";
